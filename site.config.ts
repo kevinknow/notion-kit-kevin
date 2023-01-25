01880039f484a44e4c7df62272da4c0aa6b8a8a1
Zhenchao Qi <60191124+kevinknow@users.noreply.github.com>
2023-01-25 19:10:51 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,22 +3,22 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '7875426197cf461698809def95960ebf',
-//f70d452674f1458f8584276faa06dccd //7875426197cf461698809def95960ebf//c2b4123d3fe74aefa2717fe3b3306cad//
+
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: false,
+  rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Zhenchao Qi',
-  domain: 'Kevin’s Blog',
-  author: 'Kevin Chi',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: '网站持续迁移中',
+  description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  //twitter: 'transitive_bs',
-  github: 'kevinknow',
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
@@ -26,12 +26,12 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: false,
-  defaultPageCover: true,
+  defaultPageIcon: null,
+  defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: false,
+  isPreviewImageSupportEnabled: true,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
@@ -46,7 +46,7 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: true,
+  pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
